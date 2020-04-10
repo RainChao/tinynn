d3fe03264ec2c2cc8dd1f1960f3ba0c08785f0eb
renwenchao <renwenchao@baidu.com>
2020-04-10 11:25:03 +0800

    thirdparty depends

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ done
 
 echo "src files:" $cpp_file
 
-/opt/compiler/gcc-8.2/bin/g++ -o tinynn_main $cpp_file -std=c++2a -lpthread -I. -I thirdparty/gflags/include/ -I thirdparty/gtest/include/ -L thirdparty/gflags/lib/ -L thirdparty/gtest/lib/ -lgflags -lgtest -lgtest_main
+/opt/compiler/gcc-8.2/bin/g++ -o tinynn_main $cpp_file -std=c++2a -lpthread -I src/ -I thirdparty/gflags/include/ -I thirdparty/gtest/include/ -L thirdparty/gflags/lib/ -L thirdparty/gtest/lib/ -lgflags -lgtest -lgtest_main
